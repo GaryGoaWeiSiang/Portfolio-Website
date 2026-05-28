d239f0b831642b2ed2e57d980b9cf0b3adc1ee30
GaryGoaWeiSiang <goaweisiang@graduate.utm.my>
2026-05-28 17:34:49 +0800

    Upload website content

---
+++ website.js
@@ -0,0 +1,40 @@
+
+function scrollToTop(){
+    window.scrollTo(0,0);
+}
+
+const header = document.querySelector(".header");
+const navigation = document.querySelector(".navigation");
+const subtitle = document.querySelector(".subtitle");
+const navDivider1 = document.querySelector(".navDivider1");
+const navDivider2 = document.querySelector(".navDivider2");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 30){
+        navDivider1.classList.add("collapse");
+        navigation.classList.add("collapse");
+        navDivider2.classList.add("collapse");
+    }
+    else if(window.scrollY <= 10){
+        navDivider1.classList.remove("collapse");
+        navigation.classList.remove("collapse");
+        navDivider2.classList.remove("collapse");
+    }
+
+    if(window.scrollY > 5){
+        header.classList.add("shrink");
+    }
+    else if(window.scrollY < 30){
+        header.classList.remove("shrink");
+    }
+
+    if(window.scrollY > 5){
+        subtitle.classList.add("disappear");
+    }
+    else if(window.scrollY < 15){
+        subtitle.classList.remove("disappear");
+    }
+
+});
+
